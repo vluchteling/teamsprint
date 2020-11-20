d7e53ec16487a9162b2976fddc292a4b52af640b
Alexandervsz <alexander_5560@hotmail.com>
2020-11-20 13:28:58 +0100

    code WEL gefixt

--- JsonHandler2.py
+++ JsonHandler2.py
@@ -1,14 +1,9 @@
-import urllib.request, json
+import urllib.request
+import json
 with urllib.request.urlopen("https://raw.githubusercontent.com/tijmenjoppe/AnalyticalSkills-student/master/project/data/steam.json") as url:
     data = json.loads(url.read().decode())
 file_name = 'finalAIteam1.txt'
 f = open(file_name, 'w')
 # open file in append mode
-#text = "["
-text= ""
-for i in data:
-    text += f"{i},\n"
-
-#text += "]"
 json.dump(data, f)
 f.close()
