e2f49113c45fc27343dac518c5cca16941f20286
alexander <alexander_5560@hotmail.com>
2020-12-01 11:11:58 +0100

    Niuewe API key

--- SteamAPI.py
+++ SteamAPI.py
@@ -1,7 +1,7 @@
 import urllib.request
 import json
 
-text = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=420AB17541C350F11D8B840D85F9E1B6&steamid=76561197995118880&format=json&include_appinfo=true"
+text = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=672CB8126E1C9597704B6858FA3E4F6E&steamid=76561197995118880&format=json&include_appinfo=true"
 with urllib.request.urlopen(text) as url:
     data = json.loads(url.read().decode())
 file_name = 'finalAIteam1.txt'
