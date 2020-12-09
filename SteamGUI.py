954e5bcf69e560091c5a5427c24994a59b8a19ff
alexander <alexander_5560@hotmail.com>
2020-12-09 15:49:27 +0100

    functieverbetering

--- SteamGUI.py
+++ SteamGUI.py
@@ -24,7 +24,7 @@ class SteamGUI:
         self.root.destroy()
         return
 
-    def display_naam(self):
+    def display_owned_games(self):
         """ Deze functie geeft de naam van het eerste spel uit het bronbestand weer."""
         with open('finalAIteam1.txt') as json_file:
             data = json.load(json_file)
