ac10491f4589f41d84f0b4cc3754aa77628dd767
Alexandervsz <alexander_5560@hotmail.com>
2020-12-14 13:49:53 +0100

    client communicatie geslaagd

--- SteamClientAPI.py
+++ SteamClientAPI.py
@@ -1,18 +1,49 @@
+from __future__ import print_function
 from steam.client import SteamClient
-from steam.enums.emsg import EMsg
+from steam.enums import EResult
+
+print("One-off login recipe")
+print("-" * 20)
+
+LOGON_DETAILS = {
+    'username': input("Steam user: "),
+    'password': input("Password: "),
+}
 
 client = SteamClient()
 
-@client.on(EMsg.ClientVACBanStatus)
-def print_vac_status(msg):
-    print("Number of VAC Bans: %s" % msg.body.numBans)
 
-client.cli_login()
 
-print("Logged on as: %s" % client.user.name)
-print("Community profile: %s" % client.steam_id.community_url)
-print("Last logon: %s" % client.user.last_logon)
-print("Last logoff: %s" % client.user.last_logoff)
-print("Number of friends: %d" % len(client.friends))
 
-client.logout()
+@client.on('error')
+def error(result):
+    print("Logon result:", repr(result))
+
+
+
+@client.on("logged_on")
+def handle_after_logon():
+
+    print("-" * 20)
+    print("Logged on as:", client.user.name)
+    print("Community profile:", client.steam_id.community_url)
+    print("Last logon:", client.user.last_logon)
+    print("Last logoff:", client.user.last_logoff)
+    print("Press ^C to exit")
+
+
+try:
+    result = client.cli_login(**LOGON_DETAILS)
+
+    if result != EResult.OK:
+        print("Failed to login: %s" % repr(result))
+        raise SystemExit
+
+    client.run_forever()
+except KeyboardInterrupt:
+    if client.connected:
+        print("Logout")
+        client.logout()
+
+
+
