d66bbc89e8377194c8725b6e41d1f69e2c7e473b
Alexandervsz <alexander_5560@hotmail.com>
2020-12-14 16:49:55 +0100

    code verbeterd

--- SteamClientAPI.py
+++ SteamClientAPI.py
@@ -3,28 +3,23 @@ from steam.client import SteamClient
 from getpass import getpass
 from steam.enums import EResult
 
-print("One-off login recipe")
-print("-" * 20)
 
 LOGON_DETAILS = {
     'username': input("Steam user: "),
     'password': getpass("password please: "),
+    # VERGEET IN CONFIGURATION "emulate terminal in output console" aan te doen!
 }
 
 client = SteamClient()
 
 
-
-
 @client.on('error')
 def error(result):
-    print("Logon result:", repr(result))
-
+    #print("Logon result:", repr(result))
 
 
 @client.on("logged_on")
 def handle_after_logon():
-
     print("-" * 20)
     print("Logged on as:", client.user.name)
     print("Community profile:", client.steam_id.community_url)
@@ -45,6 +40,3 @@ except KeyboardInterrupt:
     if client.connected:
         print("Logout")
         client.logout()
-
-
-
