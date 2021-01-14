26dfbdd11ff6766a2a94707af1ede5e4d8ecbc6b
alexander <alexander_5560@hotmail.com>
2021-01-14 22:38:13 +0100

    Eerste basis voor AI

--- AI.py
+++ AI.py
@@ -2,6 +2,11 @@ import os
 from tkinter import *
 from tkinter.font import Font
 from tkinter.ttk import Treeview
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from pandas import DataFrame
+
 from SteamWebAPI import SteamWebAPI
 
 
@@ -22,29 +27,49 @@ class DataScherm:
         self.gamesframe = Frame()
 
         self.gamesframe.pack()
-        friendsdata = self.haal_data_op()
+        friendsdata, friendsdict = self.haal_data_op()
+
+        speeltijdlist = []
         for friend in friendsdata:
+
             try:
                 games = friendsdata[friend]['response']['games']
-                #self.toon_data(games)
-                self.maak_data(games)
+                counter = 0
+                totaaltijd = 0
+                for game in games:
+                    counter += 1
+                    totaaltijd += game['playtime_forever']
+
+                speeltijdlist.append([friendsdict[friend], totaaltijd/counter])
+                # self.toon_data(games)
+
             except KeyError:
                 continue
+        self.maak_data(speeltijdlist)
 
-        self.afsluitButton.pack()
+        self.afsluitButton.pack(side=BOTTOM)
 
     def haal_data_op(self):
         steamid = self.client.get_client().steam_id.as_64
         data = self.api.get_friend_list(steamid)
         friendjson = data['friendslist']['friends']
         friendlist = []
+        frienddict= {}
+        steamid = self.client.get_client().steam_id.as_64
+        gebruiker = self.api.friendstatus(steamid)
+        username = gebruiker['response']['players'][0]['personaname']
+        friendlist.append(steamid)
+        frienddict[steamid] = username
         for friend in friendjson:
             friendid = friend['steamid']
+            games = self.api.friendstatus(friend['steamid'])
+            naam = games['response']['players'][0]['personaname']
+            frienddict[friend['steamid']] = naam
             friendlist.append(friendid)
         friendgamesdict = {}
         for friend in friendlist:
             friendgamesdict[friend] = self.api.get_steam_games_from_user(friend)
-        return friendgamesdict
+        return friendgamesdict, frienddict
 
     def toon_data(self, data):
         koppen = ('appid', "name", "playtime_forever (min)")
@@ -62,12 +87,25 @@ class DataScherm:
         scrollbar.config(command=self.treeview.yview)
 
     def maak_data(self, data):
-        counter = 0
-        totaaltijd = 0
-        for game in data:
-            counter += 1
-            totaaltijd += game['playtime_forever']
-        print(f"Gemiddelde speeltijd per game: {totaaltijd / counter}")
+        # data = self.sorteer_data(data)
+
+        namenlijst = []
+        tijdlijst = []
+        for friend in data:
+            namenlijst.append(friend[0])
+            tijdlijst.append(friend[1])
+        data1 = {'usernames': namenlijst,
+                 'gemiddelde speeltijd (in min)': tijdlijst
+                 }
+        df1 = DataFrame(data1, columns=['usernames', 'gemiddelde speeltijd (in min)'])
+        figure1 = plt.Figure(figsize=(6, 5), dpi=75)
+        ax1 = figure1.add_subplot(111)
+        self.bar1 = FigureCanvasTkAgg(figure1, self.root)
+        self.bar1.get_tk_widget().pack(side=LEFT)
+        df1 = df1[['usernames', 'gemiddelde speeltijd (in min)']].groupby('usernames').sum()
+        df1.plot(kind='bar', legend=True, ax=ax1)
+        ax1.set_title('Gemiddelde speeltijd per game.')
+
 
     def stop(self):
         self.gamesframe.forget()
@@ -76,7 +114,7 @@ class DataScherm:
             self.treeview.forget()
         self.gui.open_gui(True)
         self.gui.start_sensoren(True)
-
+        self.bar1.get_tk_widget().forget()
 
     def achievements(self):
         data = self.api.get_friend_list(steamid=self.client.get_client().steam_id.as_64)
